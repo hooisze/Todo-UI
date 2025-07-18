f311abe3d3de0919c52aad6b044a264c89fc970a
HHS <hooisze0206@gmail.com>
2025-07-18 09:16:00 +0800

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     location: 'production',
     // apiUrl: 'http://localhost:8081/api'  // Production API endpoint
-    apiUrl: 'https://vmtestweb01.ger.corp.intel.com/imatchAPI/api'  // Production API endpoint
+    apiUrl: 'https://todo-api-noj5.onrender.com/api'  // Production API endpoint
   };
